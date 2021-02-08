3817d51cb7bade7883ea65534ca57918eaefb8eb
Kevin Kruelle <kkruelle2@gmail.com>
2021-02-08 13:17:25 -0500

    got through step 3 started step 4

--- js/arrays.js
+++ js/arrays.js
@@ -6,20 +6,31 @@ console.log('connected...')
 3. Each value of test scores in the array should post on a new line.
 4. Create a variable that displays number of students and print to webpage. 
 5. Print number of students to the webpage before grades and on it's own line.
+6. Create a variable to find highest score
+7. Create a variable to find lowest score
+8. Create a variable to find average score
+9. Create a variable to find median score
 
 */
 
-let studentScores = [98, 97, 96, 57, 67, 87, 68, 96, 95, 98, 97, 96, 95, 94, 96, 66, 76, 76, 86, 92]
+let studentScores = [98, 97, 96, 57, 67, 87, 68, 96, 95, 98, 97, 96, 95, 94, 96, 66, 76, 46, 86, 22]
+
 
 let numOfStudents = studentScores.length;
-let main = `Students: ${numOfStudents}<br>`
+main = `Students: ${numOfStudents}<br>`
+
+let highestScore = Math.max(...studentScores);
+let lowestScore = Math.min(...studentScores);
+let sum = studentScores.reduce((previous, current) => current += previous)
+let avg = sum / studentScores.length;
 
 
 for (let i = 0; i < studentScores.length; i++) {
-    main += `Grade: ${studentScores[i]}<br>`
+        main += `Grade: ${studentScores[i]}<br>`
 }
 
-document.getElementById('output').innerHTML += main
+
+document.getElementById('output').innerHTML += `${main}<br>Highest Score: ${highestScore}<br> Lowest Score: ${lowestScore}<br> Average score: ${avg} `
 
 
 
