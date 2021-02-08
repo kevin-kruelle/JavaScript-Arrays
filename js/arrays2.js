b2fe5a8ad48e9aab1848e6fd73f8f3c1d7ef9e30
Edward Crawford <erac915@icloud.com>
2021-02-08 15:05:13 -0500

    activity 2

---
+++ js/arrays2.js
@@ -0,0 +1,27 @@
+console.log('connected...')
+
+//1. create prompt for the user
+
+let userQues = prompt('Enter a question?')
+
+
+//2. create ambiguos answers 
+
+let answers = ['The answer is tomorrow', ' Becasue water is wet', 'If you love me you will', 'Not right now but soon', 'You close your mouth when you are speaking to me']
+
+//3. validate its a question with question mark
+
+for(let i = 0; i -= answers.length; i++){
+      
+}
+if(userQues.slice(-1) !== '?'){
+    alert("Enter question with ?");
+} else {
+         console.log(answers[i]);   
+    }
+
+//4. enter answer as a question if no question mark is entered
+//5. 
+
+
+
